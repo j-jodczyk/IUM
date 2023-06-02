ea2ad32078a08b4f43fe8847f88cd49bbc1ef4d5
fpawlows <filip.pawlowski001@gmail.com>
2023-06-02 22:17:21 +0200

    Final version of tuning

--- microservice/tuning.py
+++ microservice/tuning.py
@@ -12,6 +12,7 @@ import logging
 config_logging("tuning")
 
 class ParametersSearchLoop (object):
+    # TODO: choose scoring
     def __init__ (self, default_model=True, prepare_data=True, estimator_params:dict=None, scoring='f1_micro', n_jobs=None):
         self.scoring = scoring
         self.n_jobs = n_jobs
@@ -29,24 +30,23 @@ class ParametersSearchLoop (object):
 
     def set_default_models(self):
         def get_kneigh():
-            leaf_size = list(range(1,20))
-            # leaf_size = list(range(1,50))
-            n_neighbors = list(range(1,10))
-            # n_neighbors = list(range(1,30))
+            leaf_size = list(range(1,50))
+            n_neighbors = list(range(1,30))
             p=[1,2]
-            # weights=['uniform', 'distance']
-            # algorithm = ['auto', 'ball_tree', 'kd_tree', 'brute']
+            weights=['uniform', 'distance']
+            algorithm = ['auto', 'ball_tree', 'kd_tree', 'brute']
             kneigh_params = {
                 'leaf_size': leaf_size, 
                 'n_neighbors': n_neighbors, 
-                'p':p
+                'p': p,
+                'weights': weights,
+                'algorithm': algorithm
                 }
-            # kneigh_params = dict(leaf_size=leaf_size, n_neighbors=n_neighbors, p=p, weights=weights, algorithm=algorithm)
             return { ModelManager(KNeighborsClassifier): kneigh_params}
         
         def get_tree():
-            max_depth = [2, 3, 5, 10, 20]
-            min_samples_leaf = [5, 10, 20, 50, 100]
+            max_depth = [int(x) for x in np.linspace(2, 110, num = 17)]
+            min_samples_leaf = [int(x) for x in np.linspace(2, 100, num = 15)]
             criterion = ["gini", "entropy"]
             
             tree_params = {
@@ -57,9 +57,9 @@ class ParametersSearchLoop (object):
             return { ModelManager(DecisionTreeClassifier): tree_params}
         
         def get_forest():
-            n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 5)]
+            n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
             max_features = ['auto', 'sqrt']
-            max_depth = [int(x) for x in np.linspace(10, 110, num = 5)]
+            max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
             max_depth.append(None)
             min_samples_split = [2, 5, 10]
             min_samples_leaf = [1, 2, 4]
@@ -87,15 +87,13 @@ class ParametersSearchLoop (object):
         self.X = data_df.drop(["premium_user"], axis=1)
         self.y = data_df.loc[:, "premium_user"]
 
-    # TODO: change to __next__
-    # TODO: add verbose and PArallel
     def grid_generator(self):
         for model_manager, params in self.estimator_params.items():
-            # TODO: choose scoring, change to model_manager back
             # model_manager can be passed directly to the grid, so it will log values and update accuracy if set up correctly, but due to time consumption it is not configured that way 
             grid = HalvingRandomSearchCV(model_manager.model, params, scoring=self.scoring, cv=ShuffleSplit(
                                     test_size=0.3, n_splits=5, random_state=42),
-                                verbose=2, n_jobs=-1, n_candidates="exhaust", factor=3
+                                    n_jobs=-1, n_candidates="exhaust", factor=3,
+                                    # verbose=2
                                     )
             try:   
                 grid.fit(self.X, self.y)
@@ -103,7 +101,6 @@ class ParametersSearchLoop (object):
             except KeyboardInterrupt:
                 message = f"PROGRAM STOPPED: Best params: {grid.best_params_}\tBest score: {grid.best_score_}\tBest Estimator: {grid.best_estimator_}"
                 logging.info(message)
-                print(message)
 
             yield grid.best_params_, grid.best_score_, grid.best_estimator_
                 
